9c9e31fa27050e528756e42fa84b53c3c5007ba6
Наиль Акчурин <golumvicerlid35@gmail.com>
2019-10-17 20:46:29 +0600

    init commit create table sql

---
+++ Dz/Table/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ADONET
+{
+    class Program
+    {
+        const string connectionString = "Server=A-305-08;Database=Test;Trusted_Connection=True;";
+
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = connection.CreateCommand())
+            {
+                string query = $"drop table if exists gruppa;" +
+                  $"" +
+                  $"create table gruppa (" +
+                  $"Id bigint Primary Key not null," +
+                  $"Name nvarchar(50) not null" +
+                  $")";
+
+                command.CommandText = query;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
